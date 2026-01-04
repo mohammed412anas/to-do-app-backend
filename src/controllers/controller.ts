09b8242f7c4dba67dbc3009952b866eddc173101
mohammed412anas <mohammed.anas@everest.engineering>
2026-01-05 04:34:33 +0530

    Add getTasks function to send get task request to services and send response to client

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,13 +1,22 @@
 import { Request, Response } from "express";
-import { addItem } from "../services/service";
+import { addItem, getItems } from "../services/service";
+import { ResponseType } from "../services/servicesTypes";
 
 export const addTask = async (req: Request, res: Response) => {
   try {
     const { task } = req.body;
-    const response = await addItem(task);
+    const response = await addItem(task) as ResponseType;
     res.status(response.statusCode).contentType("json").json(response);
   } catch (error) {
     res.status(500).send(error);
   }
 };
 
+export const getTasks = async (req: Request, res: Response) => {
+  try {
+    const response = (await getItems()) as ResponseType;
+    res.status(response.statusCode).contentType("json").json(response);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
