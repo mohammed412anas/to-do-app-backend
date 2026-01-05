c17a445c4d381193ec201a24e1da159a060f5163
mohammed412anas <mohammed.anas@everest.engineering>
2026-01-06 05:27:05 +0530

    Uninstall cors and @types/corse package

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,13 @@
 import express, { Express } from "express";
-import cors, { CorsOptions } from "cors";
+
 import dotenv from "dotenv";
 import router from "./routes/router";
 
 dotenv.config();
 
 const app: Express = express();
-const corsOptions: CorsOptions = {
-  origin: true,
-  methods: ["GET", "PUT", "POST", "DELETE"],
-};
 
 app.use(express.json());
-app.use(cors(corsOptions));
 app.use(router);
 
 const PORT = process.env.PORT || 10000;
