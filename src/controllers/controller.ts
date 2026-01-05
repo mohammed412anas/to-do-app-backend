1894d32e9c2dfd2be3691cdd3dbb0d55d8b375d5
mohammed412anas <mohammed.anas@everest.engineering>
2026-01-06 05:24:30 +0530

    Add deletetTask function to send delete task request to services and send response to client

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
-import { addItem, editItem, getItems } from "../services/service";
+import { addItem, deleteItem, editItem, getItems } from "../services/service";
 import { ResponseType } from "../services/servicesTypes";
 
 export const addTask = async (req: Request, res: Response) => {
   try {
     const { task } = req.body;
-    const response = await addItem(task) as ResponseType;
+    const response = (await addItem(task)) as ResponseType;
     res.status(response.statusCode).contentType("json").json(response);
   } catch (error) {
     res.status(500).send(error);
@@ -25,6 +25,19 @@ export const editTask = async (req: Request, res: Response) => {
   try {
     const { task } = req.body;
     const response = (await editItem(task)) as ResponseType;
+    res.status(response.statusCode).contentType("json").json(response);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const { taskId } = req.params;
+    {
+      String(taskId);
+    }
+    const response = (await deleteItem(taskId)) as ResponseType;
     res.status(response.statusCode).contentType("json").json(response);
   } catch (error) {
     res.status(500).send(error);
