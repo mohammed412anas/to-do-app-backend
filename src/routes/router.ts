de4456721b6f9609cf75db139c6513791e7782ea
mohammed412anas <mohammed.anas@everest.engineering>
2026-01-05 12:21:14 +0530

    Add router to get a put request to edit a task from the client

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,10 @@
 import express,{ IRouter } from "express";
-import { addTask, getTasks } from "../controllers/controller";
+import { addTask, editTask, getTasks } from "../controllers/controller";
 
 const router:IRouter = express.Router();
 
 router.post(`/addTask`,addTask);
-router.get(`/gettasks`, getTasks);
+router.get(`/getTasks`, getTasks);
+router.put(`/editTask`, editTask);
 
 export default router;
