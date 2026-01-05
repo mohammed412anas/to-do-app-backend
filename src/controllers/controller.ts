b8722d7f577e32aa14b9f81b045ff99491055b58
mohammed412anas <mohammed.anas@everest.engineering>
2026-01-05 12:04:26 +0530

    Add editTask function to send edit task request to services and send response to client

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { addItem, getItems } from "../services/service";
+import { addItem, editItem, getItems } from "../services/service";
 import { ResponseType } from "../services/servicesTypes";
 
 export const addTask = async (req: Request, res: Response) => {
@@ -15,6 +15,16 @@ export const addTask = async (req: Request, res: Response) => {
 export const getTasks = async (req: Request, res: Response) => {
   try {
     const response = (await getItems()) as ResponseType;
+    res.status(response.statusCode).contentType("json").json(response);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const editTask = async (req: Request, res: Response) => {
+  try {
+    const { task } = req.body;
+    const response = (await editItem(task)) as ResponseType;
     res.status(response.statusCode).contentType("json").json(response);
   } catch (error) {
     res.status(500).send(error);
