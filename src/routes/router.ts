fe74f6022356d9bd8d8131a85137bbaa40a1eb08
mohammed412anas <mohammed.anas@everest.engineering>
2026-01-06 05:25:28 +0530

    Add router to get a delete request to delete a task from the client

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,10 +1,10 @@
 import express,{ IRouter } from "express";
-import { addTask, editTask, getTasks } from "../controllers/controller";
+import { addTask, deleteTask, editTask, getTasks } from "../controllers/controller";
 
 const router:IRouter = express.Router();
 
 router.post(`/addTask`,addTask);
 router.get(`/getTasks`, getTasks);
 router.put(`/editTask`, editTask);
-
+router.delete(`/deleteTask/:taskId`, deleteTask);
 export default router;
